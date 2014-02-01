469162c9ede0f38a19e7de4cab854ec564e4c25d
Jochen Guck <jochenguck@web.de>
2014-02-01 19:34:26 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/.name
    	.idea/compiler.xml
    	.idea/misc.xml
    
    + Test ipl LoginService

--- src/main/java/com/gprojekt/untitledchat/services/LoginServiceImpl.java
+++ src/main/java/com/gprojekt/untitledchat/services/LoginServiceImpl.java
@@ -1,9 +1,13 @@
 package com.gprojekt.untitledchat.services;
 
 import com.gprojekt.untitledchat.dao.UserDAO;
+import com.gprojekt.untitledchat.dto.CreateUserMessage;
+import com.gprojekt.untitledchat.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.websocket.Session;
 
 /**
  * Created with IntelliJ IDEA.
@@ -22,5 +26,20 @@ public class LoginServiceImpl implements LoginService {
     @Transactional
     public boolean login(String username, String password) {
         return false;
+    }
+
+    @Override
+    public void createUser(CreateUserMessage message, Session session) {
+
+    }
+
+    @Override
+    public void needChallenge(String username, Session session) {
+
+    }
+
+    @Override
+    public void login(String signedGUID, User user) {
+
     }
 }
--- src/test/java/com/gprojekt/untitledchat/services/LoginServiceImplTest.java
+++ src/test/java/com/gprojekt/untitledchat/services/LoginServiceImplTest.java
@@ -1,5 +1,8 @@
 package com.gprojekt.untitledchat.services;
 
+import com.gprojekt.untitledchat.dao.UserDAO;
+import com.gprojekt.untitledchat.entities.User;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,6 +13,38 @@ import org.junit.Test;
  * To change this template use File | Settings | File Templates.
  */
 public class LoginServiceImplTest {
+
+    private class UserFakeDAO implements UserDAO {
+
+        @Override
+        public User persist(User user) {
+            return null;
+        }
+
+        @Override
+        public void remove(User user) {
+
+        }
+
+        @Override
+        public User getByUsername(String username) {
+            return null;
+        }
+
+        @Override
+        public User getById(long id) {
+            return null;
+        }
+    }
+
+    private LoginServiceImpl loginService;
+
+    @Before
+    public void setup() {
+        this.loginService = new LoginServiceImpl();
+        this.loginService.userDAO = null;
+    }
+
     @Test
     public void testLogin() throws Exception {
 
